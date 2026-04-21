3edcd6bb469e9dbecf8ddd9e722ed5a0bfb7f9c0
Kyle Javier <kyrnjvr@gmail.com>
2026-04-21 17:38:24 +0930

    Challenge 1: Script that prints name and current year to console

---
+++ day1/script.js
@@ -0,0 +1,5 @@
+// challenge 1: write a script that prints your name and the current year to the console
+const name = "Kyle Javier";
+const currentYear = new Date().getFullYear();
+
+console.log(`My name is ${name} and the current year is ${currentYear}`);
